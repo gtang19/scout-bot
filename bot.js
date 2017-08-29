a6696da622662ffd5660efa55c5a1bf9e8a3f6bf
Gavin Tang <gtang13@gmail.com>
2017-08-28 19:33:03 -0500

    Testing number

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-       
+
         args = args.splice(1);
         switch(cmd) {
             // hecking bean
@@ -43,6 +43,12 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'It worked'
+                });
+            break;
+            case 1:
+                bot.sendMessage({
+                  to: channelID,
+                  message: 1+1
                 });
             break;
             // Just add any case commands if you want to..
