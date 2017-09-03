876a6222e82b7dcf25abcc9d61fb25ff66f4b266
Gavin Tang <gtang13@gmail.com>
2017-09-03 17:50:06 -0500

    added some comments and stuff

--- bot.js
+++ bot.js
@@ -22,66 +22,64 @@ bot.on('ready', function (evt) {
 var testVariable = (1);
 
 bot.on('message', function (user, userID, channelID, message, evt) {
+  //Allows the bot to behave differently based on what room it is in
+  if (channelID == 353938633641951251){ //test-channel
+        // Our bot needs to know if it will execute a command
+        // It will listen for messages that will start with `!`
+        if (message.substring(0, 1) == '!') {               //commands starting with '!'
+            var args = message.substring(1).split(' ');     // args splits up each word after the '!'
+            var cmd = args[0];                              // the first word becomes the command
 
-  /*bot.sendMessage({
-    to: channelID,
-    message: testVariable++
-  }); */
+            args = args.splice(1);                          // the command is removed from the arguments list
+            switch(cmd) {
+                case 'test':
+                    bot.sendMessage({
+                      to:channelID,
+                      message: message
+                    });
+                    console.log('test')
+                break;
+                case 'ping':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'pong'
+                    });
+                case 'resetTest':
+                    testVariable = 1
+                break;
+                case 'add':
+                     bot.sendMessage({
+                       to:channelID,
+                       message: (testVariable++)
+                     });
+                break;
+                case 'what':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: (testVariable)
+                    });
+                break;
+                case 'channelID': //Displays channel ID
+                    bot.sendMessage({
+                        to: channelID,
+                        message: channelID
+                    });
+                break;
+                case 'github':
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'https://github.com/gtang19/scout-bot'
+                    });
+                break;
+                case 'shoop':
+                    bot.sendMessage({
+                      to: channelID,
+                      message: ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+                    });
 
-
-
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-
-        args = args.splice(1);
-        switch(cmd) {
-            // hecking bean
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'pong'
-                });
-            case 'resetTest':
-                testVariable = 1
-            break;
-            case 'loop':
-                while(testVariable < 3){
-                 bot.sendMessage({
-                   to:channelID,
-                   message: (testVariable++)
-                 });
-               }
-            break;
-            case 'what':
-                bot.sendMessage({
-                    to: channelID,
-                    message: (testVariable)
-                });
-            break;
-            case 'test':
-                bot.sendMessage({
-                    to: channelID,
-                    message: '!test'
-                });
-
-            break;
-            case 'github':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'https://github.com/gtang19/scout-bot'
-                });
-            break;
-            case 'shoop':
-                bot.sendMessage({
-                  to: channelID,
-                  message: ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-                });
-
-			break;
-            // Just add any case commands if you want to..
-         }
-     }
+    			break;
+                // Just add any case commands if you want to..
+             } //end of switch
+         } //commands starting with '!'
+      } //test-channel
 });
